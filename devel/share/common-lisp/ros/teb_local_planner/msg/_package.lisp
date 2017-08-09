54dd646c63480c2546153b23788ae2af1ed8786e
Andreas Ragen Ayal <andreas.ayal@ut.ee>
2017-08-09 16:59:17 +0300

    Day2

---
+++ devel/share/common-lisp/ros/teb_local_planner/msg/_package.lisp
@@ -0,0 +1,13 @@
+(cl:defpackage teb_local_planner-msg
+  (:use )
+  (:export
+   "<TRAJECTORYPOINTMSG>"
+   "TRAJECTORYPOINTMSG"
+   "<FEEDBACKMSG>"
+   "FEEDBACKMSG"
+   "<OBSTACLEMSG>"
+   "OBSTACLEMSG"
+   "<TRAJECTORYMSG>"
+   "TRAJECTORYMSG"
+  ))
+
